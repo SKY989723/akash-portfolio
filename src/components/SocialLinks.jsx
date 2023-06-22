f95418d494a8ed724d6c851611a81f520be1832d
Akash Vaishnav <104577018+SKY989723@users.noreply.github.com>
2023-06-22 07:31:05 +0530

    Update SocialLinks.jsx

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -51,11 +51,11 @@ const SocialLinks = () => {
         },
     ]
   return (
-    <div className='hidden lg:flex flex-col top-[35%] left-0 fixed'>
+    <div className=' lg:flex flex-col top-[35%] left-0 fixed'>
       <ul>
        {links.map(({id,child,href,style,download})=>(
         <li key={id} className={
-              "flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300 bg-gray-500" +
+              "flex justify-between items-center w-28 md:w-40 h-10 md:h-14 px-1 md:px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300 bg-gray-500" +
               " " +
               style
             }>
